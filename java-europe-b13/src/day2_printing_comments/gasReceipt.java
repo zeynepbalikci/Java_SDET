75a44ae294cf91a30e6dc337735fa5e8ed269753
Kadir BALIKCI <kadir-balikci@la3eb.com>
2022-07-15 19:58:24 +0200

    first commit

---
+++ java-europe-b13/src/day2_printing_comments/gasReceipt.java
@@ -0,0 +1,23 @@
+package day2_printing_comments;
+
+public class gasReceipt {
+
+	public static void main(String[] args) {
+		
+		System.out.println("+------------------------------+");
+		System.out.println("|                              |");
+		System.out.println("|        MCLEAN STORE          |");
+		System.out.println("|                              |");
+		System.out.println("|   2019-10--06-19   04:38PM   |");  
+		System.out.println("|                              |");
+		System.out.println("|   Gallons:       10.870      |");
+		System.out.println("|   Price/gallon: $ 2.089      |");
+		System.out.println("|                              |");
+		System.out.println("|   Fuel total:  $ 22.71       |");
+		System.out.println("|                              |");
+		System.out.println("+------------------------------+");
+		
+
+	}
+
+}
---
+++ java-europe-b13/src/day2_printing_comments/triangle.java
@@ -0,0 +1,19 @@
+package day2_printing_comments;
+
+public class triangle {
+
+	public static void main(String[] args) {
+		
+		System.out.println("    *");
+		
+		System.out.println("   * *");
+		
+		System.out.println("  *   *");
+		
+		System.out.println(" *     *");
+		
+		System.out.println("*********");
+
+	}
+
+}
