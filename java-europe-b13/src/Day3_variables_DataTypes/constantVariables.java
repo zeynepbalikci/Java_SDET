fd5e7b6cd16faad9416a5be2bc48f74c48b34f95
Kadir BALIKCI <kadir-balikci@la3eb.com>
2022-07-16 23:52:43 +0200

    n

---
+++ java-europe-b13/src/Day3_variables_DataTypes/constantVariables.java
@@ -0,0 +1,23 @@
+package Day3_variables_DataTypes;
+
+public class constantVariables {
+
+	public static void main(String[] args) {
+		
+		final double PI = 3.14;
+		final String BIRTHDAY="12/12/1992";
+		final int CAPACITY=50;
+		
+		int A=4;
+		String zeynep="BALIKÇI";
+		long l3=5000786785978567956L;
+		
+		System.out.println(A + "\n" + zeynep + "\n" + l3);
+		
+		
+		
+		
+
+	}
+
+}
