4e92073f41d5ac88c25532d52180206acd196a05
zeynepbalikci <kadir.balikci@mobimeo.com>
2022-07-18 21:08:11 +0200

    aritmetik

---
+++ java-europe-b13/src/Day4_arithmeticOperators/additionConcat.java
@@ -0,0 +1,25 @@
+package Day4_arithmeticOperators;
+
+public class additionConcat {
+
+	public static void main(String[] args) {
+		
+		// Number + Number = Addition
+		System.out.println(2+2);
+		System.out.println(200+20);
+		
+		// Number + String = Concatenation
+		System.out.println(5 + "Hello");
+		
+		// String + Boolenan = Concatenation
+		System.out.println("hello" + true);
+		
+		// String + String = Concatenation
+		System.out.println("hello" + "world");
+		
+		
+		
+
+	}
+
+}
---
+++ java-europe-b13/src/Day4_arithmeticOperators/task8_variablePractice.java
@@ -0,0 +1,13 @@
+package Day4_arithmeticOperators;
+
+public class task8_variablePractice {
+
+	public static void main(String[] args) {
+		
+		int year = 1492;
+		System.out.println("In "+ year +" Colombus Sailes The Oceanblue");
+		
+
+	}
+
+}
