d290f08895660c92758753475b26f2404d4092d6
Kadir BALIKCI <kadir-balikci@la3eb.com>
2022-07-19 14:53:42 +0200

    today

---
+++ java-europe-b13/src/Day4_arithmeticOperators/contribution.java
@@ -0,0 +1,15 @@
+package Day4_arithmeticOperators;
+
+public class contribution {
+
+    public static void main(String[] args) {
+
+        double montlyPay=6000;
+        double contribution;
+
+        //Calculate display 5% contribution
+        contribution=montlyPay*0.05;
+        System.out.println();
+
+    }
+}
--- java-europe-b13/src/Day4_arithmeticOperators/discount.java
+++ java-europe-b13/src/Day4_arithmeticOperators/discount.java
@@ -3,6 +3,16 @@ package Day4_arithmeticOperators;
 public class discount {
 
 	public static void main(String[] args) {
+
+		int regularPrice=59;
+		double discount;
+		double salesPrice;
+
+		discount= regularPrice*0.2;
+		salesPrice= regularPrice-discount;
+
+
+
 		
 		
 
