0681ca70728d9f35d488fe07e898a0cb7bb0a20b
Kadir BALIKCI <kadir-balikci@la3eb.com>
2022-07-26 16:22:17 +0200

    mmm

---
+++ java-europe-b13/src/Day10_controlFollowStatement/ternaryOperator.java
@@ -0,0 +1,9 @@
+package Day10_controlFollowStatement;
+
+public class ternaryOperator {
+    public static void main(String[] args) {
+        for (int i = 0; i < ; i++) {
+            
+        }
+    }
+}
