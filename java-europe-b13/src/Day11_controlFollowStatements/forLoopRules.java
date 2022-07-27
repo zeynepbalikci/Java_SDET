ac0242a46bf0ad8e1117a152e35f95bd7818b28a
Kadir BALIKCI <kadir-balikci@la3eb.com>
2022-07-27 23:59:56 +0200

    day 11 merhaba

---
+++ java-europe-b13/src/Day11_controlFollowStatements/forLoopRules.java
@@ -0,0 +1,25 @@
+package Day11_controlFollowStatements;
+
+public class forLoopRules {
+    public static void main(String[] args) {
+
+        // Adding multiple terms to the for statement.
+
+        int x=0;
+        for (long y=0,z=4 ;x<5 && y<10 ; x++,y++){
+            System.out.print(y + " ");
+        }
+        System.out.println(x);
+
+        // Redeclaring a varaible in the init block.
+
+
+        long y = 0;
+        for (y=1,x=4; x<5 && y<10; y++,x++) {
+            System.out.println("Hello");
+        }
+
+        //
+
+    }
+}
