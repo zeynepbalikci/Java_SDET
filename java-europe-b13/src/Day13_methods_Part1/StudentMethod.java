640b73a47e39ccd19ace76963fb7c9f19f9a768f
Kadir BALIKCI <kadir-balikci@la3eb.com>
2022-07-31 06:55:56 +0200

    zeyy..

---
+++ java-europe-b13/src/Day13_methods_Part1/StudentMethod.java
@@ -0,0 +1,15 @@
+package Day13_methods_Part1;
+
+public class StudentMethod {
+    public static void main(String[] args) {
+
+        sayHello();
+
+    }
+    public static void sayHello() {
+
+        System.out.println("Inside sayHello method");
+    }
+
+
+}
