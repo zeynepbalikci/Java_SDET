76e16c31188c4bc4dffb505d31be71c291197ddc
Kadir BALIKCI <kadir-balikci@la3eb.com>
2022-08-23 16:03:21 +0200

    index

--- java-europe-b13/src/Day20_stringManipulation_part2/String_IndexOf2.java
+++ java-europe-b13/src/Day20_stringManipulation_part2/String_IndexOf2.java
@@ -9,5 +9,17 @@ public class String_IndexOf2 {
 
     int secondDash = list.indexOf("-",5);
     System.out.println(secondDash);
+
+    int thirdDash = list.indexOf("-",secondDash+1);
+    System.out.println(thirdDash);
+
+    int lastDash = list.lastIndexOf("-");
+    System.out.println(lastDash);
+
+
+
+
+
+
   }
 }
